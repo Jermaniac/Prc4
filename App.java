49ef89184bef21ac53dd36d3a36bfe327683743f
Jermaniac <47125376+Jermaniac@users.noreply.github.com>
2019-02-25 15:24:43 +0100

    update for 2

--- App.java
+++ App.java
@@ -3,30 +3,34 @@ package practicas;
 public class App{
 public static void main(String[] args){
 	Grafo g = new Grafo (7);
+	Grafo g2 = new Grafo(8);
 	
-	g.insertarArista('S','A',"a");
-	g.insertarArista('S','C',"b");
-	g.insertarArista('S','F',"d");
-	g.insertarArista('S','D',"c");
-	
-	
-	g.insertarArista('A', 'B', "b");
+	g.insertarArista("S","A","a");
+	g.insertarArista("S","C","b");
+	g.insertarArista("S","F","d");
+	g.insertarArista("S","D","c");
 	
 	
-	g.insertarArista('B', 'B', "b");
-	g.insertarArista('B','F',"c,d");
-	
-	g.insertarArista('C', 'D', "a");
-	
-	g.insertarArista('D', 'S', "a");
-	g.insertarArista('D', 'E', "c");
-	
-	g.insertarArista('E', 'F', "b");
-
+	g.insertarArista("A","B", "b");
 	
 	
-
+	g.insertarArista("B", "B", "b");
+	g.insertarArista("B","F","c,d");
+	
+	g.insertarArista("C", "D", "a");
+	
+	g.insertarArista("D", "S", "a");
+	g.insertarArista("D", "E", "c");
+	
+	g.insertarArista("E", "F", "b");
+	
 	g.representar();
+	//Segundo grafo ejemplo
+	g2.insertarArista("p0", "p1", "a");
+	g2.representar();
+	
+
+	
 }
 }
 
